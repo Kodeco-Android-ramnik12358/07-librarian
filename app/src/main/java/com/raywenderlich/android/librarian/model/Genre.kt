925ebc1ded46682641786b1cd21b4420047182a4
Niklas Rammerstorfer <rammerstorfer.n@gmail.com>
2026-04-05 14:09:52 +0200

    Added `Genre` to the stored database entities.

--- app/src/main/java/com/raywenderlich/android/librarian/model/Genre.kt
+++ app/src/main/java/com/raywenderlich/android/librarian/model/Genre.kt
@@ -35,11 +35,15 @@
 package com.raywenderlich.android.librarian.model
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 import java.util.*
 
 @Parcelize
+@Entity(tableName = "genres")
 class Genre(
+    @PrimaryKey
     val id: String = UUID.randomUUID().toString(),
     val name: String
 ) : Parcelable
